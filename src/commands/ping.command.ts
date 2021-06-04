2d79bea579ca55c734de5936c8bb6f9a89e9178f
Halil Safa SAĞLIK <safa@rubiklabs.com>
2021-06-04 20:34:22 +0300

    Configured autolinter.

--- src/commands/ping.command.ts
+++ src/commands/ping.command.ts
@@ -5,7 +5,7 @@ export default {
   description: 'Returns the message delivery time.',
   method: async (message: Message, command: string, args: Array<string>) => {
     try {
-        message.reply(`Ping : **${message.client.ws.ping}ms!`)
+      message.reply(`Ping : **${message.client.ws.ping}ms!`)
     } catch (err) {
       message.reply('The command could not be processed.')
     }
